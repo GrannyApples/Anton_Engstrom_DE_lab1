333e0eb58d1fca96b2cfa2265713a272d5309cf9
Anton Engstrom <anton.engstrom1994@gmail.com>
2025-10-10 00:18:25 +0200

    refact: updated it into functions to make it dry

--- lab1.py
+++ lab1.py
@@ -1,34 +1,58 @@
-file = "dna_raw_complicated.txt"
-dna_seq = {}
-key = []
-#reads a file and fills it in a hashset by goin over it line by line
-with open(file) as f:
-    for line in f:
-        #removes whitespace
-        line = line.strip()
-        if line.startswith(">"):
-            #skips first index of seq line(in this case the > .) so the dict key reads seq# instead of >seq#...
-            key = line[1:]
-            dna_seq[key] = []
-        else:
-            dna_seq[key].append(line)
+import matplotlib.pyplot as plt
 
-print(dna_seq)
-#count how many of each letter there are in each value key pair
-counts={}
-
-for seq, lines in dna_seq.items():
-    lineCount = []
-    #loops thru the dict, and then the value of the key and then each char in the value, prob is a one line to do this with an import or something?
-    for line in lines:
-        count={}
-        line = line.upper()
-        for char in line:
-            #adds it to the count if it alrdy exists in the dict, else it creates the start of the count
-            if char in count:
-                count[char] +=1
+def Read_File(file):
+    dna_seq = {}
+    key = []
+    #reads a file and fills it in a hashset by goin over it line by line
+    with open(file) as f:
+        for line in f:
+            #removes whitespace
+            line = line.strip()
+            if line.startswith(">"):
+                #skips first index of seq line(in this case the > .) so the dict key reads seq# instead of >seq#...
+                key = line[1:]
+                dna_seq[key] = []
             else:
-                count[char]=1
-        lineCount.append(count) #adds a new value to the key if it has multiple lines.
-    counts[seq] = lineCount #adds all the line values to the current key.
-print(counts)
+                dna_seq[key].append(line)
+
+    return dna_seq
+    
+
+#count how many of each letter there are in each value key pair
+def Letter_count(dna_seq):
+    counts={}
+
+    for seq, lines in dna_seq.items():
+        lineCount = []
+        #loops thru the dict, and then the value of the key and then each char in the value, prob is a one line to do this with an import or something?
+        for line in lines:
+            count={}
+            line = line.upper()
+            for char in line:
+                #adds it to the count if it alrdy exists in the dict, else it creates the start of the count
+                if char in count:
+                    count[char] +=1
+                else:
+                    count[char]=1
+            lineCount.append(count) #adds a new value to the key if it has multiple lines.
+        counts[seq] = lineCount #adds all the line values to the current key.
+
+    return counts
+
+def draw_Plot(dna_count):
+
+
+    return
+
+
+file1 = "dna_raw.txt"
+dna1 = Read_File(file1)
+dna1_count= Letter_count(dna1)
+print(dna1_count)
+
+input("\nPress ENTER to show dna seq 2...\n")
+
+file2 = "dna_raw_complicated.txt"
+dna2 = Read_File(file2)
+dna2_count = Letter_count(dna2)
+print(dna2_count)
